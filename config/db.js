10b18add3efba28f94aa44121de6d3876fc42741
ALAMIN761740 <abdullahalamin761740@gmail.com>
2025-11-15 10:29:13 +0600

    Update and deploy

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "./db/.env";
 
+dotenv.config();
 
  export const connectdb = async () => {
   try {
-    await mongoose.connect('mongodb+srv://abdullah_al_amin:j7ToVtUoH6RTZL8A@cluster0.hqppjhd.mongodb.net/client_10_server?appName=Cluster0');
+    console.log(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("MongoDB Connected Successfully!");
   } catch (error) {
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://server-10-w3lm.onrender.com/", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
